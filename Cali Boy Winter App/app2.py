bc1a764b91700caf7974cd84433525d3865dd7a8
DK <dhernan881@gmail.com>
2019-11-03 16:06:01 -0500

    updating index2

--- Cali Boy Winter App/app2.py
+++ Cali Boy Winter App/app2.py
@@ -1,23 +1,80 @@
 from flask import Flask, render_template, redirect, request, url_for
 import requests
 
+# UNCOMMENT THIS WITH API KEY WHEN RUNNING FR
+# key =
+
+def weatherAPICall(key, latitude, longitude, save=False, weatherPath=None):
+# takes in darksky API key, and coordinates of city
+# (+ is north/east, - is south/east)
+# returns currently and hourly dicts of data, as well as weatherDict which is
+# the all of the data from the call
+# see https://darksky.net/dev/docs for specifics on parsing the dics
+# saves new call to json unless otherwise specified
+	apiURL = f"https://api.darksky.net/forecast/{key}/{latitude},{longitude}"
+	response = requests.get(apiURL)
+	weatherDict = response.json()
+	currently = weatherDict["currently"]
+	hourly = weatherDict["hourly"]
+	if save:
+		saveWeatherData(weatherPath, weatherDict)
+	return (currently, hourly, weatherDict)
+
+def citiesLoadFromJSON(citiesJSON):
+    # loads cities dictionary
+    with open(citiesJSON, "r") as file:
+        citiesDict = json.load(file)
+        print(citiesDict)
+        return citiesDict
+def caliBoyWinterStringify(currently):
+    #takes in the currently weather data dictionary and outputs our final string
+	temp = currently["temperature"]
+	precipChance = currently["precipProbability"]
+	windSpeed = currently["windSpeed"]
+	precipType = "rain" if temp > 32 else "snow"
+	out = f"It is currently {temp} degrees out. "
+	out += f"There is a {precipChance} percent chance of "
+	out += f"{precipType}, "
+	out += f"with wind speeds of {windSpeed} miles per hour."
+    out = f"It is currently {temp} degrees out. There is a {precipChance} percent chance of {precipType}, with wind speeds of {windSpeed} miles per hour."
+    qZips = (70 - temp) // 10
+    skiGear = True if temp <= 32 else False
+    rainGear = True if precipType == "rain" else False
+    out += "\n Based on this we reccomend:\n"
+    out += f"{qZips} Patagonia Quarter-Zips for warmth\n"
+    if skiGear:
+        out += "Parka, gloves, and thermals for the frigid temperature\n"
+    if rainGear:
+        out += "Docs and a baseball cap for the rain (cause who needs unbrellas)\n"
+
+	return out
+
+def wrapper(key, city):
+    citiesDict = citiesLoadFromJSON("./cities.json")
+    lat = citiesDict[city][0]
+    long = citiesDict[city][1]
+    currently = weatherAPICall(key, lat, long)
+    caliString = caliBoyWinterStringify(currently)
+    return caliString
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index(city=None):
     if(city == None):
-        return render_template('index2.html')
+        return render_template('index.html', )
     else:
-        # do actual stuff here
-        return render_template('index2.html', **locals())
+        caliString = wrapper(key, city)
+        return render_template('index.html', city=city, caliString=caliString)
 
 @app.route('/', methods=['POST'])
 def indexPost():
     city = request.form["citydropdown"]
 
-    # So I think we can do the scripting part in the index() method after
-    # a city is inputted
+
+
     return index(city)
 
 if __name__ == "__main__":
-	app.run(port="5000")
+	app.run(port="8001")
